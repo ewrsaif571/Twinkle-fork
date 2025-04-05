c337e62ab628bd70d5868b077e828dc5263945e5
ewrsaif571 <saifewr6@gmail.com>
2025-04-05 12:53:51 +0600

    Update loadData.js

--- bot/login/loadData.js
+++ bot/login/loadData.js
@@ -7,7 +7,7 @@ module.exports = async function (api, createLine) {
 	console.log(chalk.hex("#f5ab00")(createLine("DATABASE")));
 	const controller = await require(path.join(__dirname, '..', '..', 'database/controller/index.js'))(api); // data is loaded here
 	const { threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, sequelize } = controller;
-	log.info('DATABASE', getText('loadData', 'loadThreadDataSuccess', global.db.allThreadData.filter(t => t.threadID.toString().length > 15).length));
+	log.info('DATABASE', getText('loadData', 'loadThreadDataSuccess', global.db.allThreadData.filter(t => t.threadID?.toString()?.length > 15)?.length || 0))
 	log.info('DATABASE', getText('loadData', 'loadUserDataSuccess', global.db.allUserData.length));
 	if (api && global.GoatBot.config.database.autoSyncWhenStart == true) {
 		console.log(chalk.hex("#f5ab00")(createLine("AUTO SYNC")));
