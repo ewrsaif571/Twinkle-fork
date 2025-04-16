3fbe56aa08d5b5b86384999091ab9e8aafb2332b
ewrsaif571 <saifewr6@gmail.com>
2025-04-16 10:06:50 +0600

    Update ping.js

--- scripts/cmds/ping.js
+++ scripts/cmds/ping.js
@@ -18,8 +18,8 @@ module.exports = {
   },
   onStart: async function ({ api, event, args }) {
     const timeStart = Date.now();
-    await api.sendMessage("𝗖𝗵𝗲𝗰𝗸𝗶𝗻𝗴 𝗕𝗼𝘁"𝘀 𝗽𝗶𝗻𝗴...", event.threadID);
+    await api.sendMessage("𝗖𝗵𝗲𝗰𝗸𝗶𝗻𝗴 𝗕𝗼𝘁'𝘀 𝗽𝗶𝗻𝗴...", event.threadID);
     const ping = Date.now() - timeStart;
-    api.sendMessage(`>🎀 𝗧𝗵𝗲 𝗰𝘂𝗿𝗿𝗲𝗻𝘁 𝗽𝗶𝗻𝗴 𝗶𝘀  ${ping}ms.`, event.threadID);
+    api.sendMessage(`>🎀 𝗕𝗮𝗯𝘆 𝘆𝗼𝘂'𝗿 𝗰𝘂𝗿𝗿𝗲𝗻𝘁 𝗽𝗶𝗻𝗴 𝗶𝘀  ${ping}ms.`, event.threadID);
   }
 };
